b7067a9424dc60234357ceedc3e80b684ab11b84
debuka <bukasondean@gmail.com>
2023-10-10 12:59:50 -0700

    update

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 """retrieves elements from a list like c"""
 
 def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
+    if idx < 0 or idx > (len(my_list) -1):
         return (None)
     return (my_list[idx])
